b282efa22c2ab69e39506bf7b3273f7a6406723c
Lucas Henrique Nogueira <lucas.henrique2022@engenharia.ufjf.br>
2024-03-17 17:37:29 -0300

    adicionando notas da aula01_20240311

--- Aula01_20240317/comandosBasicosR.R
+++ Aula01_20240317/comandosBasicosR.R
@@ -2,4 +2,17 @@
 
 1 * 5
 
-4 + 4
+4 + 44
+
+# plot feito
+plot(faithful)
+
+# ajuste linear pelos pontos do grafico
+faith.ln <- lm(waiting ~eruptions, data = faithful)
+
+#grafico de reta
+
+abline(faith.ln)
+
+# ponto qualquer
+points(x = 3, y = 70, pch = 3, cex = 2, col = 'yellow')
