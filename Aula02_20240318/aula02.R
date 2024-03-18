7ba7f3e8dac662197db63f3837ca42cd43c6a9cd
Lucas Henrique Nogueira <lucas.henrique2022@engenharia.ufjf.br>
2024-03-18 11:58:09 -0300

    operacoes em r

---
+++ Aula02_20240318/aula02.R
@@ -0,0 +1,74 @@
+                  #   ----------------------------------------
+                  #   EST-064 - Laboratorio de Estatisitica
+                  #   Aula 02: 18/03/2024 - R como calculadora
+                  #   ----------------------------------------
+                  
+  # Adição e subtração
+                  
+5 + 4
+
+5 + 4.5  
+
+5 - 4
+
+  # Divisão e multiplicação
+
+11 * 1
+
+12 / pi
+
+45 / (9 * 3)
+
+ # Potenciação e raiz quadrada
+
+2 ** 2
+
+2 ^ 8
+
+sqrt(2)
+
+sqrt(36)
+
+  # Notação científica
+
+2e3
+
+3e-2
+
+  # Exponencial e logaritmo
+
+exp(1)
+
+exp(-2)
+
+log(1)
+
+log(2)
+
+log10(10e15)
+
+log2(8)
+
+log(49) / log(7)
+
+log(49, base = 7)
+
+log(49, 7)
+
+  # Módulo
+
+4 %% 2
+
+12 %% 5
+
+7 * 4 / 11 %% 10
+
+ #Comando floor e ceiling (truncamento e arredondamento pra cima)
+
+floor(3.14)
+
+ceiling(3.14)
+
+28 / 11 %% 10
+
+28/11 - 10*floor(28/11)
